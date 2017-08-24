bf33ec5b3e3cd3c4aa133fc2eb5ed2d8e30a625e
Sally Bradfield <salzeo@gmail.com>
2017-08-23 21:36:40 -0600

    Corrected bad note in server.js

--- server.js
+++ server.js
@@ -19,14 +19,14 @@ app.use(express.static('views'));
 
 //When working on local machine, uncomment this and comment the stuff below
 
-// db.sequelize.sync().then(function(){
-//   app.listen(port, function(){
-//     console.log("Server listening on " + port);
-//   });
-// });
-
-//When deploying to heroku.  Until we get stull working, comment above and uncomment below
-
-app.listen(port, function(){
-  console.log("Server listening on " + port);
+db.sequelize.sync().then(function(){
+  app.listen(port, function(){
+    console.log("Server listening on " + port);
+  });
 });
+
+//When deploying to heroku.  Until we get mysql working, comment above and uncomment below
+
+// app.listen(port, function(){
+//   console.log("Server listening on " + port);
+// });
