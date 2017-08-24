c829a104050775d235a598a79313375d1e55f8b7
Sally Bradfield <salzeo@gmail.com>
2017-08-24 11:40:34 -0600

    apiroutesupdated2

--- models/userfound.js
+++ models/userfound.js
@@ -1,13 +1,13 @@
 module.exports = function(sequelize, DataTypes){
 var UserFound = sequelize.define("user_found", {
   foundName: DataTypes.STRING,
-  email: DataTypes.STRING,
-  phoneNumber: DataTypes.STRING,
+  emailfound: DataTypes.STRING,
+  phoneFound: DataTypes.STRING,
   addressFound: DataTypes.STRING,
   typeofAnimal: DataTypes.STRING,
   dateFound: DataTypes.STRING,
   genderFound: DataTypes.STRING,
-  addlInfo: DataTypes.STRING,
+  addlInfofound: DataTypes.STRING,
   photoFound: DataTypes.STRING
 
 });
--- routing/apiroutes.js
+++ routing/apiroutes.js
@@ -3,8 +3,8 @@
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
-var friendData = require("../app/data/friends");
-
+var uselost = require("./models");
+var userfound = require("./models");
 // ===============================================================================
 // ROUTING
 // ===============================================================================
@@ -15,14 +15,14 @@ module.exports = function(app) {
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
-  app.get("/api/friends", function(req, res) {
-    res.json(friendData);
+  app.get("/api/thepetfinder", function(req, res) {
+    res.json(thepetfinder);
   });
 
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/thepetfinder", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
-  console.log("fuck",req.body);
+  console.log("Yay!!!",req.body);
     var newFriend = req.body;
 
   });
