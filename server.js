54c7f586a627635beb4fc18e13ac5c53bd7fc2ca
Amy Volkert <amyvolkert@comcast.net>
2017-08-28 12:31:24 -0600

    progress on AWS uploads

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ var client = s3.createClient({
 	multipartUploadThreshold: 20971520, // this is the default (20 MB)
 	multipartUploadSize: 15728640, // this is the default (15 MB)
 	s3Options: {
+		s3bucket: "thepetfinderav",
 		accessKeyId: keys.s3accesskey,
 		secretAccessKey: keys.s3secretaccesskey,
 // 		// any other options are passed to new AWS.S3()
@@ -35,45 +36,48 @@ var client = s3.createClient({
 });
 
 app.use(fileUpload());
-//
-// app.get('/', function(req, res) {
-// 	res.sendFile(path.join(__dirname, '/views', 'ilost.html'));
-// });
-//
-// app.post('/upload', function(req, res) {
-// 	if (!req.files) {
-// 		return res.status(400).send('No files were uploaded.');
-// 	}
-//
-// 	// The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-// 	var sampleFile = req.files.sampleFile;
-//
-// 	// Use the mv() method to place the file somewhere on your server
-// 	sampleFile.mv('uploads/' + req.files.sampleFile.name, function(err) {
-// 		if (err) {
-// 			return res.status(500).send(err);
-// 		}
-//
-// 		// Upload to S3
-// 		var params = {
-// 			localFile: 'uploads/' + req.files.sampleFile.name,
-//
-// 			s3Params: {
-// 				Bucket: keys.s3bucket,
-// 				Key: req.files.sampleFile.name, // File path of location on S3
-// 			},
-// 		};
-// 		var uploader = client.uploadFile(params);
-// 		uploader.on('error', function(err) {
-// 			console.error("unable to upload:", err.stack);
-// 			res.status(500).send(err.stack);
-// 		});
-// 		uploader.on('end', function() {
-// 			console.log("done uploading");
-// 			res.send('File uploaded!');
-// 		});
-// 	});
-// });
+// handling the upload
+// getting the file from ifound.html
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, '/views', 'ifound.html'));
+});
+
+// placeholder for getting the file from ilost.html
+
+// posting the file to the /uploads folder
+app.post('/upload', function(req, res) {
+	if (!req.files) {
+		return res.status(400).send('No files were uploaded.');
+	}
+
+	// The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+	var photoFound = req.files.photoFound;
+
+	// Use the mv() method to place the file somewhere on your server
+	photoFound.mv('uploads/' + req.files.photoFound.name, function(err) {
+		if (err) {
+			return res.status(500).send(err);
+		}
+	// Upload to S3
+		var params = {
+			localFile: 'uploads/' + req.files.photoFound.name,
+
+			s3Params: {
+				Bucket: keys.s3bucket,
+				Key: req.files.photoFound.name, // File path of location on S3
+			},
+		};
+		var uploader = client.uploadFile(params);
+		uploader.on('error', function(err) {
+			console.error("unable to upload:", err.stack);
+			res.status(500).send(err.stack);
+		});
+		uploader.on('end', function() {
+			console.log("done uploading");
+			res.send('File uploaded!');
+		});
+	});
+});
 
 //When working on local machine, uncomment this and comment the stuff below
 db.sequelize.sync().then(function(){
