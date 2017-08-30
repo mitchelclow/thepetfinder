6c7a462e389b5c8968fb49d4ceb8d5ee2e3bf06e
Amy Volkert <amyvolkert@comcast.net>
2017-08-30 17:16:37 -0600

    removed keys.js from .gitignore

---
+++ keys.js
@@ -0,0 +1,7 @@
+var keys = {
+	"s3bucket": "thepetfinderav",
+	"s3accesskey": "AKIAJIEKGWT7TW6NU7SQ",
+	"s3secretaccesskey": "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",
+};
+
+module.exports = keys;
