db3a2bc5384873f653995be3df04ad083cf135f5
Amy Volkert <amyvolkert@comcast.net>
2017-10-05 10:42:14 -0600

    continue with AWS authentication

---
+++ src/app/index.js
@@ -0,0 +1,5 @@
+// Require React
+// import React from 'react';
+// import ReactDom from 'react-dom';
+import React from 'react';
+import ReactDom from 'react-dom';
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,38 @@
+// The node module to get correct directory as well as other things
+var path = require('path');
+// All of the settings for webpack
+// Looks at all of the source JS, JSX, or ES2106 and combine it into one file to reduce http requests
+// Transforms any JS, JSX, or ES2016 code into vanilla JS and outputs it all into one specified file and look at CSS
 module.exports = {
-  // Example setup for your project:
-  // The entry module that requires or imports the rest of your project.
-  // Must start with `./`!
-  entry: './models/server.js',
-  // Place output files in `./dist/my-app.js`
-  output: {
-    path: 'dist',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.json$/,
-        loader: 'json'
-      }
-    ]
-  }
+
+    // The first JS script file (root) for the app
+    entry: path.resolve(__dirname, 'src') + '/app/index.js',
+    // Configuring the output after bringing all of the app/index.js requires and bring them together
+    // Webpack automatically creates the 'dist' (distribution) folder when it runs
+    output: {
+        path: path.resolve(__dirname, 'dist') + '/app',
+        // Creates a bundle.js file with all code transformed into one file in the dist/app folder
+        filename: 'bundle.js',
+        // References the index file as '/app/' for the public path
+        publicPath: '/app/'
+    },
+    // Configures babel stuff
+    module: {
+        loaders: [
+            {
+                // Looks for any file in 'src' with a .js extension and converts them into normal JS
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                loader: 'babel-loader',
+                query: {
+                    // Tells which code to convert to JS
+                    presets: ['react', 'env']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    }
 };
