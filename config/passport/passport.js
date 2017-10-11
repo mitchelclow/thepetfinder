7d9a401690b3f7358d512d1be4838646038c4f11
Amy Volkert <amyvolkert@comcast.net>
2017-10-11 04:55:07 -0600

    completed authentication

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -37,38 +37,102 @@ module.exports = function(passport, user) {
           // generateHash function ends
           };
           // Checking to see if the user exists and if not adds user with User.create
-            User.findOne({
-              where: {
+          User.findOne({
+            where: {
+              email: email
+            }
+
+          }).then(function(user) {
+            if (user)
+              {
+                return done(null, false, {
+                  message: 'That email is already taken'
+                });
+              } else {
+                var userPassword = generateHash(password);
+                var data =
+                    {
+                        email: email,
+                        password: userPassword,
+                        firstname: req.body.firstname,
+                        lastname: req.body.lastname
+                    };
+                User.create(data).then(function(newUser, created) {
+                    if (!newUser) {
+                        return done(null, false);
+                    }
+                    if (newUser) {
+                        return done(null, newUser);
+                    }
+                });
+              };
+            });
+        // LocalStrategy signup ends
+      }));
+
+      //LOCAL SIGNIN
+passport.use('local-signin', new LocalStrategy(
+
+    {
+
+        // by default, local strategy uses username and password, we will override with email
+
+        usernameField: 'email',
+
+        passwordField: 'password',
+
+        passReqToCallback: true // allows us to pass back the entire request to the callback
+
+    },
+
+
+    function(req, email, password, done) {
+
+        var User = user;
+        // isValidPassword compares  the password entered with the bCrypt comparison method
+        var isValidPassword = function(userpass, password) {
+
+            return bCrypt.compareSync(password, userpass);
+
+        }
+
+        User.findOne({
+            where: {
                 email: email
-              }
+            }
+        }).then(function(user) {
 
-            }).then(function(user) {
-              if (user)
-                {
-                  return done(null, false, {
-                    message: 'That email is already taken'
-                  });
-                } else {
-                  var userPassword = generateHash(password);
-                  var data =
-                      {
-                          email: email,
-                          password: userPassword,
-                          firstname: req.body.firstname,
-                          lastname: req.body.lastname
-                      };
-                  User.create(data).then(function(newUser, created) {
-                      if (!newUser) {
-                          return done(null, false);
-                      }
-                      if (newUser) {
-                          return done(null, newUser);
-                      }
-                  });
-                };
-              });
-        }));
+            if (!user) {
+
+                return done(null, false, {
+                    message: 'Email does not exist'
+                });
+
+            }
+
+            if (!isValidPassword(user.password, password)) {
+
+                return done(null, false, {
+                    message: 'Incorrect password.'
+                });
+
+            }
 
 
+            var userinfo = user.get();
+            return done(null, userinfo);
+
+
+        }).catch(function(err) {
+
+            console.log("Error:", err);
+
+            return done(null, false, {
+                message: 'Something went wrong with your Signin'
+            });
+
+        });
+            // LocalStrategy signin ends
+          }));
 // module.exports ends
 };
