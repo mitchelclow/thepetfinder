2c951d503ad1597091ca18cd47363a8f626f39a4
Amy Volkert <amyvolkert@comcast.net>
2017-10-17 18:45:06 -0600

    modules functioning

--- views/js/newadoptmodal.js
+++ views/js/newadoptmodal.js
@@ -2,30 +2,49 @@
 var modal = document.getElementById('myModal');
 
 // Get the register modal
-// var modal = document.getElementById('yourModal');
+var modalTwo = document.getElementById('myModalTwo');
 
 // Get the button that opens the login modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("loginBtn");
 
-// Get the button that opens the regisiter modal
-var btn = document.getElementById("yourBtn");
+// Get the button that opens the register modal
+var btnTwo = document.getElementById("registerBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
+var spanTwo = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the login button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
+}
+
+// When the user clicks on the register button, open the modal
+btnTwo.onclick = function() {
+    modalTwo.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
+  $("#show-user").html(firstname);
+// When the user clicks on <span> (x), close the modal
+spanTwo.onclick = function() {
+    modalTwo.style.display = "none";
+}
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modalTwo.style.display = "none";
+//     }
+// }
